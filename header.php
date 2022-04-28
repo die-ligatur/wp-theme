ef2b07f2b1db8bf71acf7c317fc2a71178042e58
wavedeck <david@wavedeck.io>
2022-04-28 11:46:45 +0200

    initial commit

---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+</head>
+
+<body>
+    <header id="main-header">
+
+        <div class="logo">
+            <div class="logo__img">
+                <img src="#" alt="Die Ligatur">
+            </div>
+            <div class="logo__text">
+                Die Ligatur
+            </div>
+        </div>
+
+        <div class="account">
+            <svg viewBox="0 0 448 512">
+                <path d="M224 256c70.7 0 128-57.31 128-128s-57.3-128-128-128C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3C77.61 304 0 381.6 0 477.3c0 19.14 15.52 34.67 34.66 34.67h378.7C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304z" />
+            </svg>
+        </div>
+
+        <div class="menutoggle">
+            <svg viewBox="0 0 448 512">
+                <path d="M0 96C0 78.33 14.33 64 32 64H416C433.7 64 448 78.33 448 96C448 113.7 433.7 128 416 128H32C14.33 128 0 113.7 0 96zM0 256C0 238.3 14.33 224 32 224H416C433.7 224 448 238.3 448 256C448 273.7 433.7 288 416 288H32C14.33 288 0 273.7 0 256zM416 448H32C14.33 448 0 433.7 0 416C0 398.3 14.33 384 32 384H416C433.7 384 448 398.3 448 416C448 433.7 433.7 448 416 448z" />
+            </svg>
+        </div>
+        
+    </header>
